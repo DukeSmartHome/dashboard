09d70fe10132e138eebae8c8710b40cb2f14ac35
Rvshi <harveyyshi@gmail.com>
2015-09-06 19:38:26 -0400

    Added Bus Tracker + UI Changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(function () {
+    var weatherRefresh = 30000,
+        clockRefresh = 30000,
+        busRefresh = 30000;
+
     function startTime() {
         var today = new Date();
         var h = today.getHours();
@@ -10,7 +14,7 @@ $(function () {
         $('#clock').html(h + ":" + m + "<span>" + ampm + "</span>");
         var t = setTimeout(function () {
             startTime()
-        }, 30000);
+        }, clockRefresh);
     }
 
     function checkTime(i) {
@@ -20,42 +24,99 @@ $(function () {
         return i;
     }
 
-    function getTimes() {
+
+    /* Route Info
+    C1 = 4006684   #e2000f
+    C1 weekends = 4007026   #e2000f
+    CCX = 4005486   #f9b120
+    CCX weekends = 4007030   #bae053
+    
+    Stop Info
+    Swift -> West = 4157330
+    Swift -> East = 4151494
+    Swift @ Faber = 4173498
+    
+    */
+
+    var names = {
+        "4006684": "C1",
+        "4007026": "C1",
+        "4005486": "CCX",
+        "4007030": "CCX"
+    }
+    var colors = {
+        "4006684": "rgba(226,0,15,",
+        "4007026": "rgba(226,0,15,",
+        "4005486": "rgba(249,177,32,",
+        "4007030": "rgba(186,224,83,"
+    }
+
+    function getBuses() {
         var output = $.ajax({
-            url: "https://transloc-api-1-2.p.mashape.com/arrival-estimates.json?agencies=176&callback=call&routes=4000421%2C4000592%2C4005122&stops=4002123%2C4023414%2C4021521", // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
-            type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-            data: {}, // Additional parameters here
+            url: "https://transloc-api-1-2.p.mashape.com/arrival-estimates.json?agencies=176&callback=call&routes=4006684%2C4007026%2C4005486%2C4007030&stops=4157330%2C4151494%2C4173498",
+            type: 'GET',
+            data: {},
             dataType: 'json',
-            success: function (data) {
-                //
-                //Change data.source to data.something , where something is whichever part of the object you want returned.
-                //To see the whole object you can output it to your browser console using:
-                console.log(data);
-                document.getElementById("output").innerHTML = data;
-            },
-            error: function (err) {
-                alert(err);
+            success: function (routes) {
+                updateBusDisplay(routes);
             },
             beforeSend: function (xhr) {
                 xhr.setRequestHeader("X-Mashape-Authorization", "7eLsoFnNpomshsN6Xyqfr5Xyf4aOp16e11WjsnQz1nsMDnB8YI"); // Enter here your Mashape key
             }
         });
+        var t = setTimeout(function () {
+            getBuses();
+        }, busRefresh);
     }
-    $.simpleWeather({
-        location: 'Durham, NC',
-        woeid: '',
-        unit: 'f',
-        success: function (weather) {
-           /* html = '<ul><li class="currently">' + weather.currently + '</li>';
-            html += '<li>' + weather.wind.direction + ' ' + weather.wind.speed + ' ' + weather.units.speed + '</li></ul>';*/
-            html = '<h2><i class="icon-' + weather.code + '"></i> ' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
-            html += '<div>Conditions are <span id="currently">' + weather.currently + '</span>, wind from ' + weather.wind.direction + ' at ' + weather.wind.speed + ' ' + weather.units.speed + '.</div>'
-            $("#weather").html(html);
-        },
-        error: function (error) {
-            $("#weather").html('<p>' + error + '</p>');
+
+    function updateBusDisplay(routes) {
+        var $west = $('#west .arrivals');
+        var $east = $('#east .arrivals');
+        $west.fadeOut(0);
+        $east.fadeOut(0);
+        $west.empty();
+        $east.empty();
+        for (var i = 0; i < 2; ++i) {
+            var arrivalLngth = routes.data[i].arrivals.length;
+            var whichWay = i == 0 ? $west : $east;
+            for (var j = 0; j < arrivalLngth && j < 4; ++j) {
+                // Process Bus Data
+                var arrivalTime = routes.data[i].arrivals[j].arrival_at
+                var busName = names[routes.data[i].arrivals[j].route_id];
+                var busColor = colors[routes.data[i].arrivals[j].route_id];
+                var delta = Math.round((Math.abs(new Date(arrivalTime) - new Date())) / 60000);
+                
+                var html = '<div class="arrival"><div style="background-color:' + busColor + '0.6);" class="busName">' + busName + '</div> in ' + delta + ' min</div>';
+console.log(html);
+               whichWay.append(html);
+            }
         }
-    });
+        $west.fadeIn(650);
+        $east.fadeIn(650);
+    }
+
+    function getWeather() {
+        $.simpleWeather({
+            location: 'Durham, NC',
+            woeid: '',
+            unit: 'f',
+            success: function (weather) {
+                /* html = '<ul><li class="currently">' + weather.currently + '</li>';
+                 html += '<li>' + weather.wind.direction + ' ' + weather.wind.speed + ' ' + weather.units.speed + '</li></ul>';*/
+                html = '<h2><i class="icon-' + weather.code + '"></i> ' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
+                html += '<div>Conditions are <span id="currently">' + weather.currently + '</span>, wind from ' + weather.wind.direction + ' at ' + weather.wind.speed + ' ' + weather.units.speed + '.</div>'
+                $("#weather").html(html);
+            },
+            error: function (error) {
+                $("#weather").html('<p>' + error + '</p>');
+            }
+        });
+        var t = setTimeout(function () {
+            getWeather();
+        }, weatherRefresh);
+    }
+
     startTime();
-    getTimes();
+    getBuses();
+    getWeather();
 });
